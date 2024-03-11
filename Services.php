3f6398f378b27c36ab5893ff0b5a4a26de212b9b
seifnasser1 <Seifnasser2001@gmail.com>
2024-03-11 16:30:27 +0200

    Edit

--- Services.php
+++ Services.php
@@ -13,16 +13,21 @@
     <p> We offer a wide range of products and services to meet the needs of
         athletes of all levels. Some of the things we offer include:</p>
 
-    <img src="img/logo.jpg" alt="image for padel">
-    </div>
-    <div>
-        <h4>Padel Tennis</h4>
-        <p> Is a variation of traditional tennis that is played with smaller racquets
-            and a smaller court. The game is typically played with two players or four players (two on each team)
-            and the objective is to hit the ball over the net and into the opponent's court without them being
-            able to return it.</p>
-        <a href="#page">Book Now</a>
-    </div>
+        <img src="img/logo.jpg" alt="image for padel">
+</div>
+<div>
+    <h4>Padel Tennis</h4>
+    <p> Is a variation of traditional tennis that is played with smaller racquets
+        and a smaller court. The game is typically played with two players or four players (two on each team)
+        and the objective is to hit the ball over the net and into the opponent's court without them being
+        able to return it.</p>
+    <a href="#page">Book Now</a>
+</div>
+<div>
+    <h4>Squash</h4>
+    <p>Squash is a racket sport that is played in a four-walled court with a small, hollow rubber ball. The objective of the game is to hit the ball against the front wall in such a way that your opponent is unable to return it.</p>
+    <a href="#page">Book Now</a>
+</div>
 </body>
 
 </html>
